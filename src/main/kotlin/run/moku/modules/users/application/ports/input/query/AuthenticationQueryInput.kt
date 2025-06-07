5855c1fa51a8bd1b92883dca8fd13d1ba92ab951
lkdcode <dlrlejr1@naver.com>
2025-06-07 22:52:33 +0900

    Refactor(:user) security 분리

--- src/main/kotlin/run/moku/modules/users/application/ports/input/query/AuthenticationQueryInput.kt
+++
@@ -1,7 +0,0 @@
-package run.moku.modules.users.application.ports.input.query
-
-import run.moku.framework.security.auth.AuthenticationDTO
-
-interface AuthenticationQueryInput {
-    fun loadUserDetails(username: String): AuthenticationDTO
-}
